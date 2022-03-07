87d7e1e61e698175e90d52036acff413b3fb3de7
surekhad588 <101106676+surekhad588@users.noreply.github.com>
2022-03-07 18:06:07 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+package com.firstproject;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+	int n1=0,n2=1,n3,count=50;
+	for(int i=2;i<count;i++){
+		n3=n1+n2;
+		System.out.println(""+n3);
+		n1=n2;
+		n2=n3;
+		
+	}
+
+	}
+
+}
